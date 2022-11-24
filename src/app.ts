f6a2f889114d066e2def68d33ef034e7e1718a79
David Soards <david@soards.me>
2022-11-24 14:24:07 -0500

    disable morgan

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import cors from 'cors';
+import express, { Request } from 'express';
 import * as dotenv from 'dotenv';
-import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 
 import api from './api';
 import * as middlewares from './middlewares';
@@ -11,11 +11,12 @@ import { MessageResponse } from './interfaces';
 dotenv.config();
 
 const app = express();
-
-app.use(morgan('dev'));
+// morgan.token('body', (req: Request) => JSON.stringify(req.body));
+// app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.post('/hook', (req, res) => {
   console.log(req.body); // Call your action on the request here
   res.status(200).end(); // Responding is important
